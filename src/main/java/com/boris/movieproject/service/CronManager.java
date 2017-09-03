d8dfae72e670324bfaa380bdbe5c67b1d7254b5e
Boris <KhatulMadan@users.noreply.github.com>
2017-09-03 23:53:46 +0300

    03/09/17

--- src/main/java/com/boris/movieproject/service/CronManager.java
+++ src/main/java/com/boris/movieproject/service/CronManager.java
@@ -66,80 +66,28 @@ public class CronManager {
 
             String title = results.get(i);
             movie.setTitle(title);
-
-            //
-            System.out.println("You have choosed movie: " + title);
-
-
             int movieID = service.getID(title);
-            System.out.println("movie ID: " + movieID);
-
             String details = service.getDetails(movieID);
-            System.out.println("Full details: " + details);
-
             String credits = service.getCredits(movieID);
-            System.out.println("Json credits: " + credits);
-
             int runtime = service.getRuntime(details);
             movie.setRuntime(runtime);
-
-            System.out.println("Runtime: " + runtime);
-
-
             String genre = service.getGenre(details);
             movie.setGenre(genre);
-
-            System.out.println("Genre: " + genre);
-
-
             String release = service.getReleaseDate(details);
             movie.setRelease_date(release);
-
-            System.out.println("release date: " + release);
-
-
             String overview = service.getOverview(details);
             movie.setDescription(overview);
-
-            System.out.println("overview: " + overview);
-
-
             String filepath = service.getBackdropPath(details);
             movie.setPoster(filepath);
-
-            System.out.println("Link to poster: " + filepath);
-
             DownloadService download = new DownloadService();
             download.downloadFile(filepath);
-
-
             String director = service.getDirector(credits);
             movie.setDirector(director);
-
-            System.out.println("Director: " + director);
-
-
             String writers = service.getWriter(credits);
             movie.setWritten(writers);
-
-            System.out.println("Written by: " + writers);
-
-
             String actors = service.getCast(credits);
             movie.setCast(actors);
-
-            System.out.println("Starring: " + actors);
-
-            if (movie != null)
-            {
-                System.out.println("It really works, but need to save it somehow");
-            }
-
-             dbService.add(movie);
-
-
-
-
+            dbService.add(movie);
 
 
         }
