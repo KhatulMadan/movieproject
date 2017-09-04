4420289b89ca4bd4405e53870bf05cbee6a8abaf
Boris <KhatulMadan@users.noreply.github.com>
2017-09-04 14:47:33 +0300

    04/09/17

--- src/main/java/com/boris/movieproject/entity/Movie.java
+++ src/main/java/com/boris/movieproject/entity/Movie.java
@@ -18,14 +18,6 @@ public abstract class Movie {
                                     String cast,
                                     String description,
                                     String poster);
-    public abstract void setTitle(String title);
-    public abstract void setRuntime(int runtime);
-    public abstract void setGenre(String genre);
-    public abstract void setRelease_date(String release_date);
-    public abstract void setWritten(String written);
-    public abstract void setDirector(String director);
-    public abstract void setCast(String cast);
-    public abstract void setDescription(String description);
-    public abstract void setPoster(String poster);
+
 
 }
